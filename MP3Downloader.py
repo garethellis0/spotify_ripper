2d40cd9e99d55f68881a6b07685d3b7d05928b6e
MathewMacDougall <mmacd97@gmail.com>
2016-09-20 08:20:19 -0700

    Added best video evaluation and ability to skip existing songs - Now checks if a song already exists in the download folder and removes it   from the list of songs to download if it already exists - Added function _get_vid_info() to get title and url info of each video in song search results - Added _get_best_song_url() to evaluate best vid in search results to download. Should avoid   music videos, covers, live performances, and instrumental - Added comments for both new functions and general cleanup of other functions

--- MP3Downloader.py
+++ MP3Downloader.py
@@ -19,14 +19,42 @@ class MP3Downloader:
     # Calls all functions to download mp3 files based on song information passed in
     # as a dictionary
     def get_downloads(self):
+        self._removeExistingSongs()
         self._get_search_urls()
         self._get_song_urls()
-        self._download_songs()
+        #self._download_songs()
         self._rename_songs()
-        self._set_metadata()
+        self._write_metadata()
+
+
+    # Checks for songs that already exist in the download path, and removes them
+    # from the list of songs to be downloaded
+    def _removeExistingSongs(self):
+        print (self.songs)
+        print ("Checking for existing songs...")
+        songs_to_remove = []
+
+        # Identify songs that already exist
+        for song in self.songs:
+            song_name_regex = r"" + re.escape(song["Artist"]) + r" - " + re.escape(song["Title"]) + r"\.mp3"
+
+            for filename in os.listdir(self.path):
+                if re.match(song_name_regex, filename):
+                    print("The song \"%s - %s\" already exists. Skipping this song." %(song["Artist"], song["Title"]))
+                    songs_to_remove.append(song)
+                    break
+
+        # Remove the pre-existing songs from the songs dictionary
+        # Cannot be done in the first loop because removing items
+        # during the check for songs messes up the indexing and not all
+        # songs are properly detected
+        for song in songs_to_remove:
+            self.songs.remove(song)
+
 
     # Gets the youtube search url for each song in the dictionary,
     # and adds it to the dictionary as "search_url"
+    # Urls are created in the form: https://www.youtube.com/results?search_query=Artist+Title+lyrics
     def _get_search_urls(self):
         #The first part of every search url
         url_start = "https://www.youtube.com/results?search_query="
@@ -40,11 +68,14 @@ class MP3Downloader:
             search_url = search_url.lower()
             song["search_url"] = search_url
 
+
     # Using the youtube search url for each song in the songs dictionary,
     # gets the url for the top search result for that song and adds it to
     # the dictionary
     #
     # Youtube video urls have 11 character long unique id's
+    # Urls are created in the form: https://www.youtube.com/watch?v=XXXXXXXXXXX
+    # where "X" represents a random character part of the unique video id
     def _get_song_urls(self):
         url_beginning = "https://www.youtube.com/watch?v="
         print("Retrieving song urls...")
@@ -55,12 +86,94 @@ class MP3Downloader:
                 html = response.read()
 
             # decodes html source from binary bytes to string
-            source = html.decode("utf-8", "ignore")
+            search_source = html.decode("utf-8", "ignore")
 
-            #unique url termination of first video in search results
-            url_termination = re.findall("href=\"\/watch\?v=(.*?)\"", source)[0]
-            song_url = url_beginning + url_termination
-            song["song_url"] = song_url
+            vid_info = self._get_vid_info(search_source)
+            best_song_url_termination = self._get_best_song_url(song, vid_info)
+
+            if best_song_url_termination is "":
+                print("Unable to find a suitable video for %s - %s, skipping this song." %(song['Artist'], song['Title']))
+            else:
+                #url_terminations = re.findall("href=\"\/watch\?v=(.*?)\"", search_source)[0]
+                #video_titles = re.findall("title=\"(.*?)\"", search_source)
+                song_url = url_beginning + best_song_url_termination
+                song["song_url"] = song_url
+
+
+    # Given a youtube search url extracts the title and url termination (the 11-character long unique
+    # id at the end of each video's url) and returns the results as a list of dictionaries,
+    # with each video's info in a separate dictionary
+    # @Param search_source the web source of the youtube search page for the song to collect vid info from
+    # @Param max_num_vids the maximum number of dictionaries of vid info the function will return
+    # @Returns a list of dictionaries containing the title and url termination for each video
+    def _get_vid_info(self, search_source, max_num_vids):
+        vids_to_eval = []
+        index = 1
+
+        # Isolate the list of results in the source
+        results_source = re.split(r"<ol id=\"item-section-.*?\" class=\"item-section\">", search_source)[1]
+        results_source = re.split(r"<\/ol>\n<\/li>\n<\/ol>", results_source)[0]
+
+        # split by video in list, returns the type of entry (video, playlist, channel)
+        results_source = re.split(r"<li><div class=\"yt-lockup yt-lockup-tile yt-lockup-(.*?) vve-check clearfix.*?\"", results_source)
+
+        while len(vids_to_eval) < max_num_vids and index < len(results_source) - 1:
+            source_type = results_source[index]
+            source = results_source[index + 1]
+
+            if source_type == "video":
+                video_url = re.findall(r"href=\"\/watch\?v=(.*?)\"", source)[0]
+                video_title = re.findall(r"title=\"(.*?)\"", source)[2]
+
+                # print ("%i, %i" %(len(re.findall(r"title=\"(.*?)\" rel=\".*?\" aria-describedby=\".*?\"", source)), len(re.findall(r"href=\"\/watch\?v=(.*?)\"", source))))
+                # This regex performs inconsistently, don't know why.
+                #video_title = re.findall(r"title=\"(.*?)\" rel=\".*?\" aria-describedby=\".*?\"", source)[0]
+
+                vids_to_eval.append({
+                    "url" : video_url,
+                    "title" : video_title
+                })
+
+            index += 2
+
+        return vids_to_eval
+
+    # Given a list of vid info (video title and url termination) for a song, returns the first song in
+    # the list that is not a cover, music video, live performance, or instrumental
+    # @Param song the dictionary with the info for the song whose videos are being evaluated
+    # @Param vid_info the list of dictionaries containing info (title, url termination) for the
+    #        videos to be evaluated
+    # @Returns the url termination of the first video in the list that meets all criteria
+    #          (not a music video, live performance, cover, or instrumental),
+    #          or an empty string "" if no video meets the criteria
+    def _get_best_song_url(self, song, vid_info):
+        for vid in vid_info:
+            song_title = song['Title']
+            vid_title = vid["title"]
+            url = vid['url']
+
+            # If the video a cover (not by the artist)
+            if re.search(r"(?<![a-z])cover(?![a-z])", vid_title, "i") is not None\
+                    or re.search(re.escape(song['Artist']), vid_title, "i") is None\
+                    and re.search(r" cover ", song_title, "i") is None:
+                continue
+            # if the video is a live performance
+            elif re.search(r"(?<![a-z])live(?![a-z])", vid_title, "i") is not None\
+                    and re.search(r" live ", song_title, "i") is None:
+                continue
+            # If the video is a music video
+            elif re.search(r"music video", vid_title, "i") is not None\
+                    or re.search(r"music([^a-z])video", vid_title, "i") is not None:
+                continue
+            # If the video is an instrumental
+            elif re.search(r"(?<![a-z])instrumental(?![a-z])", vid_title, "i") is not None\
+                    and re.search(" instrumental ", song_title, "i") is None:
+                continue
+            else:
+                return url
+
+        return ""
+
 
     # For each song url in the songs dictionary, downloads the corresponding song as an mp3 file
     def _download_songs(self):
@@ -87,8 +200,11 @@ class MP3Downloader:
                 print ("Attemtping to download: %s" %url)
                 ydl.download([url])
 
+
     # Renames each downloaded song from the songs dictionary to the form "Artist - Title"
     def _rename_songs(self):
+        print ("Renaming songs...")
+
         for song in self.songs:
             # find the downloaded file in the path and rename it to the proper name
             for filename in os.listdir(self.path):
@@ -98,12 +214,11 @@ class MP3Downloader:
                 if re.match(my_regex, filename):
                     os.rename(self.path + filename, self.path + new_name)
                     song["new_name"] = new_name
-                    print("Renaming song to: %s" % new_name)
                     break
 
     # Writes title, artist, and album metadata for each downloaded song from the songs dictionary,
     # and changes file permissions so everyone has access (access code 777)
-    def _set_metadata(self):
+    def _write_metadata(self):
         print("Writing metadata...")
         for song in self.songs:
             path_to_song = self.path + song["new_name"]
--- test/MP3DownloaderAPITest.py
+++ test/MP3DownloaderAPITest.py
@@ -18,32 +18,32 @@ class TestMP3DownloaderAPI(unittest.TestCase):
     #Test dictionary includes URLs for testing
     def get_test_dictionary(self):
         songs = [
-            {
-                "Title": "Life Itself",
-                "Artist": "Glass Animals",
-                "Album": "Life Itself",
-                "Time": "04:40",
-                "URL": "https://www.youtube.com/watch?v=N3bklUMHepU"
-            },
-
-            {
-                "Title": "American Money",
-                "Artist": "BØRNS",
-                "Album": "Test",
-                "Time": "4:20",
-                "URL": "https://www.youtube.com/watch?v=ABFz2Qag6Dw"
-            },
-
-
-            {
-                "Title": "Get Right",
-                "Artist": "Jimmy Eat World",
-                "Album": "Get Right",
-                "Time": "02:49",
-                "URL": "https://www.youtube.com/watch?v=vMj7baqFV3M"
-
-            },
-
+            # {
+            #     "Title": "Life Itself",
+            #     "Artist": "Glass Animals",
+            #     "Album": "Life Itself",
+            #     "Time": "04:40",
+            #     "URL": "https://www.youtube.com/watch?v=N3bklUMHepU"
+            # },
+            #
+            # {
+            #     "Title": "American Money",
+            #     "Artist": "BØRNS",
+            #     "Album": "Test",
+            #     "Time": "4:20",
+            #     "URL": "https://www.youtube.com/watch?v=ABFz2Qag6Dw"
+            # },
+            #
+            #
+            # {
+            #     "Title": "Get Right",
+            #     "Artist": "Jimmy Eat World",
+            #     "Album": "Get Right",
+            #     "Time": "02:49",
+            #     "URL": "https://www.youtube.com/watch?v=vMj7baqFV3M"
+            #
+            # },
+            #
             {
                 "Title": "Bang Bang",
                 "Artist": "Green Day",
@@ -51,78 +51,78 @@ class TestMP3DownloaderAPI(unittest.TestCase):
                 "Time": "03:25",
                 "URL": "https://www.youtube.com/watch?v=mg5Bp_Gzs0s"
             },
-
-            {
-                "Title": "Hardwired",
-                "Artist": "Metallica",
-                "Album": "Hardwired",
-                "Time": "03:11",
-                "URL": "https://www.youtube.com/watch?v=Rqnl1Z9okE4"
-            },
-
-            {
-                "Title": "Wake Up Call",
-                "Artist": "Nothing but Thieves",
-                "Album": "Nothing But Thieves (Deluxe)",
-                "Time": "02:45",
-                "URL": "https://www.youtube.com/watch?v=8phg58HrQek"
-            },
-
-            {
-                "Title": "Rock Lobster",
-                "Artist": "The B-52's",
-                "Album": "The B-52's",
-                "Time": "6:49",
-                "URL": "https://www.youtube.com/watch?v=tG6Be3KtOZg"
-            },
-
-            {
-                "Title": "Just Can't Get Enough",
-                "Artist": "Depeche Mode",
-                "Album": "Catching Up With Depeche Mode",
-                "Time": "3:25",
-                "URL": "https://www.youtube.com/watch?v=34s_cIuHWB4"
-            },
-
-            {
-                "Title": "Red Flag",
-                "Artist": "The Moth & The Flame",
-                "Album": "test",
-                "Time": "4:20",
-                "URL": "https://www.youtube.com/watch?v=bqDrftAxYpk"
-            },
-
-            {
-                "Title": "Women",
-                "Artist": "Def Leppard",
-                "Album": "Hysteria",
-                "Time": "6:11",
-                "URL": "https://www.youtube.com/watch?v=dSZ2Q3cKepU"
-            },
-
-            {
-                "Title": "Camilla",
-                "Artist": "Basshunter",
-                "Album": "Bass Generation",
-                "Time": "3:23",
-                "URL": "https://www.youtube.com/watch?v=4__Cq-DeB5U"
-            },
-
-            {
-                "Title": "Goodbye Forever",
-                "Artist": "Volbeat",
-                "Album": "Seal The Deal & Let's Boogie",
-                "Time": "4:31",
-                "URL": "https://www.youtube.com/watch?v=WEElfat8H-I"
-            },
-
-            {
-                "Title": "Coffee Girl",
-                "Artist": "The Tragically Hip",
-                "Album": "We Are The Same",
-                "Time": "3:46",
-                "URL": "https://www.youtube.com/watch?v=A_7nPkjdLQY"
-            }
+            #
+            # {
+            #     "Title": "Hardwired",
+            #     "Artist": "Metallica",
+            #     "Album": "Hardwired",
+            #     "Time": "03:11",
+            #     "URL": "https://www.youtube.com/watch?v=Rqnl1Z9okE4"
+            # },
+            #
+            # {
+            #     "Title": "Wake Up Call",
+            #     "Artist": "Nothing but Thieves",
+            #     "Album": "Nothing But Thieves (Deluxe)",
+            #     "Time": "02:45",
+            #     "URL": "https://www.youtube.com/watch?v=8phg58HrQek"
+            # },
+            #
+            # {
+            #     "Title": "Rock Lobster",
+            #     "Artist": "The B-52's",
+            #     "Album": "The B-52's",
+            #     "Time": "6:49",
+            #     "URL": "https://www.youtube.com/watch?v=tG6Be3KtOZg"
+            # },
+            #
+            # {
+            #     "Title": "Just Can't Get Enough",
+            #     "Artist": "Depeche Mode",
+            #     "Album": "Catching Up With Depeche Mode",
+            #     "Time": "3:25",
+            #     "URL": "https://www.youtube.com/watch?v=34s_cIuHWB4"
+            # },
+            #
+            # {
+            #     "Title": "Red Flag",
+            #     "Artist": "The Moth & The Flame",
+            #     "Album": "test",
+            #     "Time": "4:20",
+            #     "URL": "https://www.youtube.com/watch?v=bqDrftAxYpk"
+            # },
+            #
+            # {
+            #     "Title": "Women",
+            #     "Artist": "Def Leppard",
+            #     "Album": "Hysteria",
+            #     "Time": "6:11",
+            #     "URL": "https://www.youtube.com/watch?v=dSZ2Q3cKepU"
+            # },
+            #
+            # {
+            #     "Title": "Camilla",
+            #     "Artist": "Basshunter",
+            #     "Album": "Bass Generation",
+            #     "Time": "3:23",
+            #     "URL": "https://www.youtube.com/watch?v=4__Cq-DeB5U"
+            # },
+            #
+            # {
+            #     "Title": "Goodbye Forever",
+            #     "Artist": "Volbeat",
+            #     "Album": "Seal The Deal & Let's Boogie",
+            #     "Time": "4:31",
+            #     "URL": "https://www.youtube.com/watch?v=WEElfat8H-I"
+            # },
+            #
+            # {
+            #     "Title": "Coffee Girl",
+            #     "Artist": "The Tragically Hip",
+            #     "Album": "We Are The Same",
+            #     "Time": "3:46",
+            #     "URL": "https://www.youtube.com/watch?v=A_7nPkjdLQY"
+            # }
 
 
         ]
